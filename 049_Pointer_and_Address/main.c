41eaccc4c99d7f3b71cb3c0550baa126fed9a342
parkseiwon <psw7347@gmail.com>
2020-08-25 16:56:54 +0900

    modified

--- 049_Pointer_and_Address/main.c
+++ 049_Pointer_and_Address/main.c
@@ -1,5 +1,5 @@
 /**
- * Pointer and Address test 1
+ * Pointer and Address test (swap)
  * 
  * File name : main.c
  * @version 1.0.0
@@ -10,6 +10,8 @@
 
 #include <stdio.h>
 
+void swap(int* x, int* y);
+
 int main()
 {
     int a = 7;
@@ -17,6 +19,19 @@ int main()
 
     printf("%d %d %p\n", a, *ptr, ptr); // 주소는 OS로 결정되어지고, 실행할 때마다 바뀜
 
+
+    int x = 100;
+    int y = 200;
+
+    swap(&x, &y);
+
+    printf("%d %d\n", x, y);
+
     return 0;
 }
 
+void swap(int* x, int* y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
